a32c0f77faff5a5bc898fccc5fea6dc9d36c3482
Cheb Da Al <34172197+p1415@users.noreply.github.com>
2021-01-26 14:37:21 -0500

    Create backpack.tf-alerts-apostrophe-fix.js

---
+++ backpack.tf-alerts-apostrophe-fix.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name Backpack.tf Alerts fix for item names with apostrophe
+// @match https://backpack.tf/stats/*
+// @icon https://backpack.tf/favicon_440.ico
+// @version 1.0
+// @namespace https://backpack.tf/u/76561197968633696 
+// @author D. Alex
+// ==/UserScript==
+
+if( typeof item_name != 'undefined' ){
+  item_name = item_name.replace(/&#039;/g,"'");
+}  
