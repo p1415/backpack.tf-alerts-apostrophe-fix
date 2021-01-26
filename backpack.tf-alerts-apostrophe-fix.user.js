f3c32a1eed28600bcc8ee9f3d0bb3bb89873e28f
Cheb Da Al <34172197+p1415@users.noreply.github.com>
2021-01-26 15:25:27 -0500

    Update backpack.tf-alerts-apostrophe-fix.user.js
    + @run-at, not sure if it matters though

--- backpack.tf-alerts-apostrophe-fix.user.js
+++ backpack.tf-alerts-apostrophe-fix.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name Backpack.tf Alerts fix for item names with apostrophe
+// @name Backpack.tf Alerts fix for items names with apostrophe
 // @match https://backpack.tf/stats/*
 // @icon https://backpack.tf/favicon_440.ico
+// @run-at document-end
 // @version 1.0
 // @namespace https://backpack.tf/u/76561197968633696 
 // @author D. Alex
